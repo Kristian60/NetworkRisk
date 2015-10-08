04bfbb99b8b67d87410a270b9320145c9d56c111
Kristian Thorup <thorup.dk@gmail.com>
2015-10-08 16:16:56 +0200

    ftj

--- Connectedness.py
+++ Connectedness.py
@@ -41,6 +41,7 @@ def EstimateVAR(data, H):
 #test223
 # TEST TEST 123
 # 1234
+# fejl tjek
 
 def EstimateVAR_slow():
     df = pd.read_csv('C:/Users/thoru_000/Dropbox/Pers/PyCharmProjects/Speciale/data.csv', sep=";")
