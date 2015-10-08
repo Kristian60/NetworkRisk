7099c071a0d85778e1aafe62abd691ebd5198e5f
ThomasHolden <Holden750@hotmail.com>
2015-10-08 16:15:10 +0200

    1234

--- Connectedness.py
+++ Connectedness.py
@@ -40,6 +40,7 @@ def EstimateVAR(data, H):
  # test
 #test223
 # TEST TEST 123
+# 1234
 
 def EstimateVAR_slow():
     df = pd.read_csv('C:/Users/thoru_000/Dropbox/Pers/PyCharmProjects/Speciale/data.csv', sep=";")
