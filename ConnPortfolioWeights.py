4d2cbe7b24dc8eccb77c6238200931dbf9a0fa96
ThomasHolden <Holden750@hotmail.com>
2015-10-08 16:36:00 +0200

    new

---
+++ ConnPortfolioWeights.py
@@ -0,0 +1,19 @@
+from __future__ import division
+
+__author__ = 'Thomas'
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import psycopg2
+from Connectedness import EstimateVAR
+
+def Main():
+
+    pass
+
+
+
+if __name__ == "__main__":
+    Main()
