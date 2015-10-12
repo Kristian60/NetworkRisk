21a392458af9302cbea6e003518da0cb1f7b4fc7
Kristian Thorup <thorup.dk@gmail.com>
2015-10-12 16:17:10 +0200

    Full period, introduced dummy and sparse estimation

--- Connectedness.py
+++ Connectedness.py
@@ -9,19 +9,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 import time
+from statsmodels.tsa.vector_ar.var_model import ma_rep
 
 
 t0 = time.time()
-
-print t0
-exit()
 
 pd.set_option('notebook_repr_html', True)
 pd.set_option('display.max_columns', 300)
 pd.set_option('display.width', 3000)
 
 
-def EstimateVAR(data, H):
+def EstimateVAR(data, H, sparse_method=False):
     """
 
     :param data: A numpy array of log returns
@@ -30,25 +28,32 @@ def EstimateVAR(data, H):
     """
 
     model = sm.VAR(data)
-    results = model.fit(maxlags=10, ic='aic')
+    results = model.fit(maxlags=H, ic='aic')
 
     SIGMA = np.cov(results.resid.T)
-    ma_rep = results.ma_rep(maxn=H)
+
+    if sparse_method==True:
+        _nAssets = results.params.shape[1]
+        _nLags = results.params.shape[0]/results.params.shape[1]
+
+        custom_params = np.where(abs(results.params / results.stderr)>1.96,results.params,0)[1:].reshape((_nLags,_nAssets,_nAssets))
+        _ma_rep = ma_rep(custom_params,maxn=H)
+    else:
+        _ma_rep = results.ma_rep(maxn=H)
 
     GVD = np.zeros_like(SIGMA)
 
     r, c = GVD.shape
     for i in range(r):
         for j in range(c):
-            GVD[i, j] = 1 / np.sqrt(SIGMA[i, i]) * sum([ma_rep[h, i].dot(SIGMA[j]) ** 2 for h in range(H)]) / sum(
-                [ma_rep[h, i, :].dot(SIGMA).dot(ma_rep[h, i, :]) for h in range(H)])
-            # GVD[i,j] = SIGMAINV[i,i] * sum([ma_rep[h,i].dot(SIGMA[j])**2 for h in range(H)]) / sum([ma_rep[h,i,:].dot(SIGMA).dot(ma_rep[h,i,:]) for h in range(H)])
+            GVD[i, j] = 1 / np.sqrt(SIGMA[i, i]) * sum([_ma_rep[h, i].dot(SIGMA[j]) ** 2 for h in range(H)]) / sum(
+                [_ma_rep[h, i, :].dot(SIGMA).dot(_ma_rep[h, i, :]) for h in range(H)])
         GVD[i] /= GVD[i].sum()
 
-    return pd.DataFrame(GVD), SIGMA, ma_rep, results.resid.T
+    return pd.DataFrame(GVD), SIGMA, _ma_rep, results.resid
 
 
-def BootstrapMult(resid, marep, iter):
+def BootstrapMult(resid, marep, iter, dummy=False):
     '''
 
     Ikke færdiggjort.
@@ -72,15 +77,18 @@ def BootstrapMult(resid, marep, iter):
         simReturns = np.zeros((periods, nAssets))
         simValues = np.ones((periods + 1, nAssets))
 
-        shockMatrix = np.array([random.choice(resid.T.values) for x in range(len(simReturns) + 15)])
-
+        shockMatrix = np.array([random.choice(resid.values) for x in range(len(simReturns) + 15)])
         impulseResponseSystem = marep[::-1]  # Invert impulse responses to fit DataFrame
-        for t in range(len(simReturns)):
-            for h in range(responseLength):
-                simReturns[t] += impulseResponseSystem[h].dot(shockMatrix[t + h - responseLength + 1])
-            simValues[t + 1] *= simValues[t] * (simReturns[t] + 1)
 
-        dailyReturns.append(simValues[-1, :].sum() / simValues.shape[1])
+        if dummy==True:
+            pseudoReturn = np.product(np.sum(shockMatrix[15:]+1,axis=1)/11)
+            dailyReturns.append(pseudoReturn)
+        else:
+            for t in range(len(simReturns)):
+                for h in range(responseLength):
+                    simReturns[t] += impulseResponseSystem[h].dot(shockMatrix[t + h - responseLength + 1])
+                simValues[t + 1] *= simValues[t] * (simReturns[t] + 1)
+            dailyReturns.append(simValues[-1, :].sum() / simValues.shape[1])
 
     return dailyReturns
 
@@ -92,22 +100,33 @@ def realizedDaily():
 
 if __name__ == "__main__":
 
-    df = pd.read_csv('data/CRSP_IndexData.csv', sep=",", nrows=100000)
-    print "data loaded", time.time() - t0
-    df = df.set_index(pd.to_datetime(df['DATE'] + ' ' + df['TIME']))
-    df = df.ix[:, 2:]
-    df = df.asfreq('1Min')
+    df = pd.read_csv('data/minutedata.csv', sep=",", index_col=0)
+    df.index = pd.to_datetime(df.index)
     df = np.log(df).diff().dropna()
+    print "data loaded", time.time() - t0
 
+
+    # Actual bootstrapped values
     actualReturns = realizedDaily()
-    con, sigma, marep, resid = EstimateVAR(df, 15)
-    modelReturns = BootstrapMult(resid, marep, 1000)
+    print "Actual returns constructed", time.time() - t0
+    sns.distplot(actualReturns, norm_hist=True, label="Actual")
 
+    # Modeling using sparse method
+    con, sigma, marep, resid = EstimateVAR(df, 15, sparse_method=True)
+    print "Model Estimation done", time.time() - t0
+    modelReturns_Sparse = BootstrapMult(resid, marep, 1000)
+    sns.distplot(modelReturns_Sparse, norm_hist=True, label="Sparse")
 
-    sns.distplot(modelReturns, norm_hist=True,label="Model")
-    sns.distplot(actualReturns, norm_hist=True,label="Actual")
-    sns.distplot(np.random.normal(np.mean(modelReturns),np.std(modelReturns),len(modelReturns)),norm_hist=True,label="Normal")
-    sns.distplot(np.random.normal(np.mean(actualReturns),np.std(actualReturns),len(actualReturns)),norm_hist=True,label="Normal2")
+    # Modeling using full MA-rep
+    con, sigma, marep, resid = EstimateVAR(df, 15, sparse_method=False)
+    print "Model Estimation done", time.time() - t0
+    modelReturns_Full = BootstrapMult(resid, marep, 1000)
+    sns.distplot(modelReturns_Full, norm_hist=True, label="Full")
+
+    # Passing through shocks as dummy data
+#    modelReturns_dummy = BootstrapMult(resid, marep, 1000, dummy=True)
+#    sns.distplot(modelReturns_dummy, norm_hist=True, label="Dummy")
+
     print time.time()-t0
     plt.legend()
     plt.show()
