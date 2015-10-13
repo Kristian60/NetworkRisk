570b377502a5413fd57a433e464468de1e50e53a
Kristian Thorup <thorup.dk@gmail.com>
2015-10-13 18:12:56 +0200

    estimateAndBootstrap, rollingEstimates

--- Connectedness.py
+++ Connectedness.py
@@ -147,6 +147,9 @@ def rollingEstimates(trainingData, realData, start, end):
     results.to_csv('results_.csv')
 
 
+
+
+
 if __name__ == "__main__":
     df = pd.read_csv('data/minutedata2.csv', sep=",", index_col=0)
     df.index = pd.to_datetime(df.index)
