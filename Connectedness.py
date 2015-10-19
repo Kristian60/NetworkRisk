21f56f1b56024e25e27950dab3d9cc52e4095783
Kristian Thorup <thorup.dk@gmail.com>
2015-10-19 17:03:47 +0200

    improved logging

--- Connectedness.py
+++ Connectedness.py
@@ -5,8 +5,8 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 import statsmodels.tsa.api as sm
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import random
 import scipy
 import time
