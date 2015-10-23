2e65577975975791216d6261dc7ab854f5a7289f
Kristian Thorup <thorup.dk@gmail.com>
2015-10-23 11:59:02 +0200

    second run at backtest

--- gitUpdate.py
+++ gitUpdate.py
@@ -1,8 +1,7 @@
 import urllib
 import os
 
-
 testfile = urllib.URLopener()
 testfile.retrieve("https://raw.githubusercontent.com/Kristian60/NetworkRisk/master/Connectedness.py", "newGit.py")
 
-os.system('nohub python newGit.py')
+os.system('python newGit.py')
