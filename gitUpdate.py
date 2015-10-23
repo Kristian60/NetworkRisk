fe0c53fb041c1337c6f4082e28ffd643e7f121fd
Kristian Thorup <thorup.dk@gmail.com>
2015-10-23 11:30:54 +0200

    GitUpdate

---
+++ gitUpdate.py
@@ -0,0 +1,8 @@
+import urllib
+import os
+
+
+testfile = urllib.URLopener()
+testfile.retrieve("https://raw.githubusercontent.com/Kristian60/NetworkRisk/master/Connectedness.py", "newGit.py")
+
+os.system('nohub python newGit.py')
