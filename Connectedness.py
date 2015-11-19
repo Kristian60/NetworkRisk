776bb74331591d886fdd88603ae33697e6376203
Kristian Thorup <thorup.dk@gmail.com>
2015-11-19 14:48:25 +0100

    Optimizations

--- Connectedness.py
+++ Connectedness.py
@@ -13,7 +13,6 @@ import datetime
 import sys
 from functools import partial
 import multiprocessing as mp
-import HelpFunctions
 
 if hasattr(sys, 'getwindowsversion'):
     it = 1000
@@ -91,7 +90,7 @@ def BootstrapMult(resid, marep, nIterations, dummy=False, decay=True):
     simR = np.empty((periods, nAssets))
     simV = np.ones((periods + 1, nAssets))
     if decay:
-        shockM = HelpFunctions.bootstrapExpDecay(resid, nIterations)
+        shockM = bootstrapExpDecay(resid, nIterations)
     else:
         shockM = np.array([[random.choice(residNp) for x in range(len(simR) + 15)] for nn in range(nIterations)])
 
@@ -112,6 +111,30 @@ def BootstrapMult(resid, marep, nIterations, dummy=False, decay=True):
             dailyReturns.append(simValues[-1, :].sum() / simValues.shape[1])
 
     return dailyReturns
+
+def bootstrapExpDecay(data, nIterations):
+    d1 = data.index[-1]
+    d1 = datetime.datetime(d1.year, d1.month, d1.day)
+
+    shapeval = 10
+    daysSince = [(d1 - j).days + 1 for j in data.index]
+
+    probDist = [1 - expon.cdf(j, scale=shapeval) for j in np.unique(daysSince)]
+    probDist /= np.sum(probDist)
+    probDist = np.cumsum(sorted(probDist, reverse=True))
+
+    minsPerDay = data.resample("d", how="count").values[:, 0]
+    utilizedLags = int(391 - minsPerDay[0])
+    bootstrapLength = 391 + utilizedLags
+
+    data = np.insert(data.values, 0, np.empty_like(data.ix[:utilizedLags, :]), axis=0)
+
+    data = data.reshape((len(data) / 391, 391, data.shape[1]))
+    uninumbers = np.random.uniform(size=(bootstrapLength, nIterations))
+
+    a = np.array([np.digitize(uninumbers[_], probDist) for _ in range(bootstrapLength)])
+    b = np.array([[random.choice(data[-i, :, :]) for i in a[:, q]] for q in range(nIterations)])
+    return b
 
 
 def realizedDaily(day=False):
