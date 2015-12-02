6b2bcdeb4f34c8a1cb5e33e5451fcfdcf7c063da
ThomasHolden <Holden750@hotmail.com>
2015-12-02 11:18:18 +0100

    get files

--- gfiles.py
+++ gfiles.py
@@ -8,14 +8,16 @@ print "Start"
 flist = sorted(os.listdir('Z:/TAQ/TAQHDF5/'))
 slist = os.listdir('data/taq/')
 
-for ff in flist:
+for ff in flist[::-1]:
     print ff
     if ff.replace('taq_', '')[:4] >= '2001' and ff.replace('taq_', '')[:4] < '2014' and str(ff).replace('taq_','').replace('h5','csv') not in slist:
         slist = os.listdir('data/taq/')
         print "Downloading..."
         t0 = datetime.datetime.now()
-        #ff = 'taq_20131231.h5'
+        ff = 'taq_20131231.h5'
+
         path = "Z:/TAQ/TAQHDF5/" + ff
+        path = "C:/Users/Thomas/Desktop/taq_19930107.h5"
         df = pd.read_hdf(path, 'Trades')
         ind = pd.read_hdf(path, 'TradeIndex')
         ind['end'] = np.cumsum(ind['count'])
@@ -30,6 +32,8 @@ for ff in flist:
         for i in ind.index:
             start = int(ind.loc[i, 'start'])
             end = int(ind.loc[i, 'end'])
+            if end not in df.index:
+                end -= 1
             df.loc[start:end, 'sym'] = ind.loc[i, 'ticker']
         df.to_csv('data/taq/' + ff.replace('taq_', '').replace('.h5', '') + '.csv', columns=['time', 'price', 'sym'],
                   index=False)
---
+++ gfilesv2.py
@@ -0,0 +1,44 @@
+__author__ = 'Thomas'
+
+import h5py
+import pandas as pd
+import datetime
+import numpy as np
+import os
+
+
+flist = sorted(os.listdir('Z:/TAQ/TAQHDF5/'))
+slist = os.listdir('data/taq/')
+
+for ff in flist[::-1]:
+    print ff
+    if ff.replace('taq_', '')[:4] >= '2001' and ff.replace('taq_', '')[:4] < '2014' and str(ff).replace('taq_','').replace('h5','csv') not in slist:
+        t0=datetime.datetime.now()
+        f = h5py.File("Z:/TAQ/TAQHDF5/" + ff, 'r')
+        symlist = 'AAPL AXP BA CAT CSCO CVX DD DIS GE GS HD IBM INTC JNJ JOM KO MCD MMM MRK MSFT NKE PFE PG TRV UNH UTX V VZ WMT XOM'.split(
+            ' ')
+        ind = pd.DataFrame(np.array(f['TradeIndex']))
+        ind['end'] = np.cumsum(ind['count'])
+        ind['ticker'] = [str(j).strip() for j in ind['ticker']]
+        ind = ind[ind['ticker'].isin(symlist)].reset_index(drop=True)
+
+        df = []
+        for i in ind.index:
+            start = int(ind.loc[i,'start'])
+            end = int(ind.loc[i,'end'])
+            df.extend(f['Trades'][start:end])
+
+        ind['count'] = np.cumsum(ind['count'])
+        fr = 0
+        df = pd.DataFrame(np.array(df))
+        for i in ind.index:
+            to = int(ind.loc[i,'count'])
+            df.loc[fr:,'sym'] = ind.loc[i,'ticker']
+            fr = to
+
+        df['time'] = pd.to_datetime(df['utcsec'], unit='s')
+        df.to_csv('data/taq/' + ff.replace('taq_', '').replace('.h5', '') + '.csv', columns=['time', 'price', 'sym'],
+                          index=False)
+
+
+        print datetime.datetime.now()-t0
